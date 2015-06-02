eb4a96f058cfe79fd574e79be2f00d4ee5b88021
yuqiqian <yuqi.qian@outlook.com>
2015-06-02 10:19:25 -0400

    Devise User complete

--- Blocmarks/db/seeds.rb
+++ Blocmarks/db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+
+admin = User.new(
+ name:     'Admin User',
+ email:    'admin@example.com',
+ password: 'helloworld'
+)
+admin.skip_confirmation!
+admin.save!
+
+# Create a moderator
+moderator = User.new(
+ name:     'normal User',
+ email:    'normal@example.com',
+ password: 'helloworld',
+)
+moderator.skip_confirmation!
+moderator.save!
+
+# Create a member
+member = User.new(
+ name:     'Yuqi',
+ email:    'yuqi.qian@outlook.com',
+ password: '88888888'
+)
+member.skip_confirmation!
+member.save!
+
+puts "Seed finished"
+puts "#{User.count} users created"
