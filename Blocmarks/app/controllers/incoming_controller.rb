33128e306f85277649db90694977019229743a41
yuqiqian <yuqi.qian@outlook.com>
2015-06-09 11:23:37 -0400

    fixing receiving email problem

--- Blocmarks/app/controllers/incoming_controller.rb
+++ Blocmarks/app/controllers/incoming_controller.rb
@@ -3,19 +3,20 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only:[:create]
 
   def create
-    
-    @user = User.find_by_email(params[:sender])
-    @topic = Topic.find_by_title(params[:subject])
+    email = params[:envelope][:sender]
+    subject = params[:message][:headers][:subject]
+    @user = User.find_by_email(email)
+    @topic = Topic.find_by_title(subject)
     #@url = params["body-plain"]
 
     if @user == nil
-      @user = User.new(email: params[:sender], password: Faker::Lorem.characters(8))
+      @user = User.new(email: email, password: Faker::Lorem.characters(8))
       @user.skip_confirmation!
       @user.save!
     end
 
     if @topic == nil
-      @topic = Topic.new(title: params[:subject], user: @user)
+      @topic = Topic.new(title: subject, user: @user)
       @topic.save!
     end
 
