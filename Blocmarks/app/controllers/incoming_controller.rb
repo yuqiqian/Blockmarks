4101e981c639b0370131ce8140415466e65357f5
yuqiqian <yuqi.qian@outlook.com>
2015-06-11 10:43:53 -0400

    receiving incoming emails add url

--- Blocmarks/app/controllers/incoming_controller.rb
+++ Blocmarks/app/controllers/incoming_controller.rb
@@ -1,5 +1,6 @@
 class IncomingController < ApplicationController
   require 'faker'
+  include ApplicationHelper
   skip_before_action :verify_authenticity_token, only:[:create]
 
   def create
@@ -7,7 +8,7 @@ class IncomingController < ApplicationController
     subject = params[:subject]
     @user = User.find_by_email(email)
     @topic = Topic.find_by_title(subject)
-    #@url = params["body-plain"]
+    @url = params["body-plain"]
 
     if @user == nil
       @user = User.new(email: email, password: Faker::Lorem.characters(8))
@@ -19,6 +20,10 @@ class IncomingController < ApplicationController
       @topic = Topic.new(title: subject, user: @user)
       @topic.save!
     end
+
+    @bookmark = @topic.bookmarks.new(url: @url)
+    @bookmark.image = get_embed_image(@bookmark.url)
+    @bookmark.save!
 
     head 200
   end
