7755a37260a61ceefd72c34319145e0abf5b11fa
yuqiqian <yuqi.qian@outlook.com>
2015-06-22 08:55:16 -0400

    Blocmetrics api added

--- Blocmarks/app/assets/javascripts/application.js
+++ Blocmarks/app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require blocmetrics
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
-//= require blocmetrics
+
 
